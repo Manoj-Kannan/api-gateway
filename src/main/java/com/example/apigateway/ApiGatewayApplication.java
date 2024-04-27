4125ef1d423529343cdad4b13ed49a333752011a
ManojKannanD <fabulousmanoj27@gmail.com>
2024-04-27 11:36:55 +0530

    configured api-gateway
    http://localhost:8765/quiz-service/quiz/get/2

--- src/main/java/com/example/apigateway/ApiGatewayApplication.java
+++ src/main/java/com/example/apigateway/ApiGatewayApplication.java
@@ -10,4 +10,12 @@ public class ApiGatewayApplication {
 		SpringApplication.run(ApiGatewayApplication.class, args);
 	}
 
+	/*
+	Say, we have multiple MicroServices (iam service, service 1, service 2, service 3)
+	To access any of the service, we need to succeed in iam authentication. (Since each service is hosted individually)
+	To use service 1, we need to login with iam
+	To use service 2, we need to login with iam
+
+	To solve this, we can use a common api-gateway which shares common authentication for all services.
+	 */
 }
